9e2cff961b1e4392f38c7bb853e427407b9a46db
Gara <rzayevgara@gmail.com>
2023-02-23 13:12:16 +0400

    Initial Commit

---
+++ Flag Game/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flag Game
+//
+//  Created by Rzayev Gara on 23.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
